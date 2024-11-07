141ba161be744d18a81ba97b2964d6300ae723e1
Liriannys19 <141455405+liriannys2007@users.noreply.github.com>
2024-11-07 08:43:04 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function criaCartao(materi, pergunta, resposta){
+    let caixa = document.getElementsByid('caixa')
+    let cartao = document.createElement('article')
+
+    cartao.className('cartao')
+    cartao.innerHTML = `
+    `
+}
