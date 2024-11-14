7ec5551520708d9cde2644bfa243b98b1f301812
Liriannys19 <141455405+liriannys2007@users.noreply.github.com>
2024-11-14 08:29:02 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,21 +1,34 @@
 criaCartao(
-    
+    "Educação Financeira",
+    "Qual é o principal objetivo da educação financeira?",
+    "Ter consciência de nossos comportamentos financeiros e entender como o dinheiro, as dívidas e a poupança funcionam"
 )
 
 criaCartao(
-
+    "Cidadania e cívismo",
+    "Exemplos de cidadania?",
+    "ato de volar, não poluir, não infringir a lei, respeitar o direito de silêncio do próximo."
 )
 
 criaCartao(
-    
-
+    "Empreendedorismo",
+    "O que significa a sigla DAS ",
+    "Documento de arrecadação do simples nacional"
 )
 
 criaCartao(
-    
+    "Geografia",
+    "Como as nações unidos pretenden promover a cooperação internacional",
+    "È evitar novos conflitos globais"
 )
 
-
 criaCartao(
-
+    "História",
+    "Em que ano e quem foi eleito o primeiro presidente do Brasil?",
+    "1891, Deodoro da Fonseca."
+)
+criaCartao(
+    "Quimica",
+    "a combustão gera quais compostos",
+    "Agua e gás carbonico"
 )
